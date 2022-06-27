cc1104ff927c33e50325a518028ebdc11e5425b1
CitriCoder <citricoder@gmail.com>
2022-06-27 17:03:06 -0300

    feat(): agregar mini navbar para admin

--- modulos/header.php
+++ modulos/header.php
@@ -16,8 +16,14 @@
 <body>
 
     <!-- HEADER -->
+    <div class="bg-dark py-0">
+        <div class="container">
+            <a class="text-white" href="admin/login.php">Login Temporal</a>
+        </div>
+    </div>
 
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top margen" id="mainNav">
         <div class="container">
             <a class="navbar-brand" href="index.php">CIIE La Costa</a>
             <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
