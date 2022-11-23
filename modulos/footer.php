ef5f88c37619da1e0e258cfd65947a3f8b9ca2d0
CitriCoder <rossettimarcos17@gmail.com>
2022-11-23 16:13:13 -0300

    fix(footer): redirigir a una nueva pestaña

--- modulos/footer.php
+++ modulos/footer.php
@@ -22,7 +22,7 @@
                 <div class="col-lg-8 col-md-10 mx-auto">
                     <ul class="list-inline text-center">
                         <li class="list-inline-item">
-                            <a href="https://twitter.com/CIIE_LaCosta">
+                            <a target="_blank" href="https://twitter.com/CIIE_LaCosta">
                                 <span class="fa-stack fa-lg">
                                     <i class="fas fa-circle fa-stack-2x"></i>
                                     <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
@@ -30,7 +30,7 @@
                             </a>
                         </li>
                         <li class="list-inline-item">
-                            <a href="https://www.facebook.com/ciie.lacosta">
+                            <a target="_blank" href="https://www.facebook.com/ciie.lacosta">
                                 <span class="fa-stack fa-lg">
                                     <i class="fas fa-circle fa-stack-2x"></i>
                                     <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
@@ -38,7 +38,7 @@
                             </a>
                         </li>
                         <li class="list-inline-item">
-                            <a href="https://www.instagram.com/ciie.lacosta/?hl=es">
+                            <a target="_blank" href="https://www.instagram.com/ciie.lacosta/?hl=es">
                                 <span class="fa-stack fa-lg">
                                     <i class="fas fa-circle fa-stack-2x"></i>
                                     <i class="fab fa-instagram fa-stack-1x fa-inverse"></i>
